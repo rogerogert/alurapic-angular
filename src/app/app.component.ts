70afafb763c194aa14376770d90641bed3981bbe
Rogerio Teixeira <rogerioteixeira91@gmail.com>
2018-12-08 20:27:26 -0200

    Aula 03: Integração com WebAPIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Bem-vindo ao AluraPic';
 
-  photos = [
-    {
-      url: 'http://animais.culturamix.com/blog/wp-content/gallery/animais-mais-inteligentes-do-mundo/5a-leao-africano.jpg',
-      description: 'Leão Africano'
-    },
-    {
-      url: 'https://www.sobiologia.com.br/figuras/Curiosidades/sentido_animais2.jpg',
-      description: 'Hamster'
-    }
-  ];
+  photos: Object[] = [];
+  constructor(private photoService: PhotoService) {}
+
+  ngOnInit(): void {
+    this.photoService
+      .listFromUser('flavio')
+      .subscribe(photos => this.photos = photos);
+  }
 }
